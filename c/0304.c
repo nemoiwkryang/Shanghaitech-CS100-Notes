11cbbea0920ae231cac1c604d1fa94468fab36bf
nemoiwkryang <yangguanran@outlook.com>
2026-03-04 09:09:19 +0800

    a

--- c/0304.c
+++ c/0304.c
@@ -3,9 +3,25 @@ the history of C
 AT&T, K&R, UNIX
 C89/ANSI, ISO C
 
+reference to cppreference.com can be found in the titles
+
 when compiling - save as .c, compile, run
 
 function
+
+<return type> <name>(<args>){ // if no args, use void
+    <Body>
+}
+
+main function - 
+int main(void, or something else){return 0;}
+
+Output and return value
+printf
+scanf, input
+
+"%d" indicates the input type (integer)
+& get the address
 */
 #include<stdio.h>
 
@@ -13,7 +29,13 @@ int f(int x){// a function, accept args and return
     return x*x;
 }
 
+double ff(double x, double y){
+    return x+y;
+}
+
 int main(void){//main function, designated start of the program
-    printf("%s","Hello world\n");
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%s %d","Hello world\n",a+b);
     return 0;
 }

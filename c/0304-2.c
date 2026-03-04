e683412240923f406e98a72f344af437a9d18644
nemoiwkryang <yangguanran@outlook.com>
2026-03-04 09:39:18 +0800

    a

--- c/0304-2.c
+++ c/0304-2.c
@@ -1,5 +1,21 @@
 /*
 variables and arithmetic types
 C has static type system (python is dynamically-typed)
+type known at compile-time
+declare a variable - its scope
 
+readability - declare right before used; meaningful names
 */
+#include<stdio.h>
+int x,y;//global
+int main(void)
+{
+    int p,q;//local
+    int max(int a,int b)
+    {
+        return a>b?a:b;
+    }
+    scanf("%d%d",&p,&q);
+    printf("%d",max(p,q));
+    return 0;
+}

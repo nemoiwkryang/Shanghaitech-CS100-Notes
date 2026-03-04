0dfbaa196bc0ae1ddae837112b7f20409a6d1c5d
nemoiwkryang <yangguanran@outlook.com>
2026-03-04 09:55:17 +0800

    a

--- c/0304-2.c
+++ c/0304-2.c
@@ -5,12 +5,17 @@ type known at compile-time
 declare a variable - its scope
 
 readability - declare right before used; meaningful names
+everytime initialize a variable when declared
+
+some basic arithmetic types
+from binary, bit, byte to types
+
 */
 #include<stdio.h>
-int x,y;//global
+//global
 int out;
 void print(void){
-    printf("%d",out);
+    printf("%d\n",out);
 }
 int main(void)
 {
@@ -19,8 +24,10 @@ int main(void)
     {
         return a>b?a:b;
     }
+    int x,y;
     scanf("%d%d",&p,&q);
     out=max(p,q);
     print();
+    printf("%d %d\n",x,y);//uninitialized value printed
     return 0;
 }

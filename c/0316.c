0f535e13f79067190abc66725822fd1bd51a01a9
nemoiwkryang <nemoiwkryang@nemoiwkryangdeMacBook-Air.local>
2026-03-16 10:07:15 +0800

    0316 Update

--- c/0316.c
+++ c/0316.c
@@ -3,14 +3,27 @@ while and for
 for (init (assignment/declaration); condition; iteration_expr)
 (equivalent to ) init; while(condition){interation}
 variables declared inside only have the scope of the loops
+
+op precedence and evaluation order
+e.g. ++i + i++, i++ + 1, ..., are undefined behavior
+see operator as function (e.g. i++ vs ++i)
+comparison ops, logical ops, ...
+"!" > cmp op > && > ||, ...
+short - circuit evaluation - a && b - a==0 -> stop; a || b - a==1 -> stop;
+?: 
+assignment operator "="
+
+do-while
+switch，case, default # case label must be an integer constant expression
+
+function
 */
 #include<stdio.h>
 int main(){
-    for (int i=0;i<n;i++){
+    int n;
+    scanf("%d",&n);
 
-    }
-    while (n--){
-
-    }
+    for (int i=0;i<n;i++){}
+    while (n--){}
     return 0;
 }

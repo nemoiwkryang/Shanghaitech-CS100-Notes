fce46cdc49ea343413265d06eed1c78445744f53
nemoiwkryang <nemoiwkryang@nemoiwkryangdeMacBook-Air.local>
2026-03-16 08:32:08 +0800

    0316 Update Rename

--- c/0304.c
+++ c/0304.c
@@ -28,18 +28,44 @@ about printf format
 
 summary - header files, main function, scanf/printf, return value, string
 */
+/*
+variables and arithmetic types
+C has static type system (python is dynamically-typed)
+type known at compile-time
+declare a variable - its scope
+
+readability - declare right before used; meaningful names
+everytime initialize a variable when declared
+
+some basic arithmetic types
+from binary, bit, byte to types
+
+*/
 #include<stdio.h>
 
 int f(int x){// a function, accept args and return
     return x*x;
 }
 
+int out;
+void print(void){
+    printf("%d\n",out);
+}
 double ff(double x, double y){
     return x+y;
 }
-
+int max(int a,int b)
+{
+    return a>b?a:b;
+}
 int main(void){//main function, designated start of the program
     int a,b;
+    int p,q;//local
+    int x,y;
+    scanf("%d%d",&p,&q);
+    out=max(p,q);
+    print();
+    printf("%d %d\n",x,y);//uninitialized value printed
     scanf("%d %d",&a,&b);
     printf("%s %d + %d = %d","Hello world\n",a,b,a+b);
     return 0;
---
+++ c/0316.c
@@ -0,0 +1,16 @@
+/*
+while and for
+for (init (assignment/declaration); condition; iteration_expr)
+(equivalent to ) init; while(condition){interation}
+variables declared inside only have the scope of the loops
+*/
+#include<stdio.h>
+int main(){
+    for (int i=0;i<n;i++){
+
+    }
+    while (n--){
+
+    }
+    return 0;
+}

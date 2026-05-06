5d044ddcb433604107603f805dad485658aba3bb
nemoiwkryang <yanggr2025@shanghaitech.edu.cn>
2026-05-06 09:19:04 +0800

    0506 Update

---
+++ cpp/0506.cpp
@@ -0,0 +1,17 @@
+/*
+typedef ElemType Alias;
+using Alias = ElemType;
+
+can be used in classes
+-> usage: ClassName::AliasName
+*/
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+int main()
+{
+    return 0;
+}

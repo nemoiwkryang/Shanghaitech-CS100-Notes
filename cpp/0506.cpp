9d972e18e574ad6c59e77af78e6473751271ce27
nemoiwkryang <yanggr2025@shanghaitech.edu.cn>
2026-05-09 21:04:57 +0800

    0509 Update

--- cpp/0506.cpp
+++ cpp/0506.cpp
@@ -45,10 +45,21 @@ function - no body - declare; with body - defination
 
 class def - declar of all members(var, func, ...)
 member func can be def-ed outside
+accessed by ClassName::MemberName
+
+class declar : class / struct Name;
+declares an incomplete type, cannot create an object, cannot access members
+declare a ptr / ref to it OK;
 
 ---
 
-more about ::
+more about destructors
+    called when end of scope / delete / termination of program
+    every class need a destructor
+    empty dtor == default, which destroy every member
+    automaticly call member destructors if class
+    when dtor function is called, members are still accessible in the function
+    dtor order == reverse of declare in class def order
 */
 
 #include<iostream>
@@ -57,20 +68,34 @@ more about ::
 
 using ll = long long;
 
+class T;
+
+class P;
+
+void printId(T &t); // declaration; ref to incompl type OK
+
+
 class T{
     static int cnt;
     int id;
     friend void printId(T&); // friend declaration, can be placed anywhere in the class def
+    std::string name;
 public:
     using ll = long long;
-    T():id(cnt++){}
+
+    T(const std::string name = "No Name"):id(cnt++), name(name){}
+
     int T_id() const;
+
     friend class P;
+
+    ~T(){
+        std :: cout << "Destruct: " << id << " " << name << std :: endl;
+    } // OK; No new / *alloc used 
+    // ~T(){}, or ~T(){/* Print the members */} Also OK
 };
 
-int T::T_id() const{
-    return this->id;
-}
+
 
 class P{
 public:
@@ -78,16 +103,13 @@ public:
         t.id = t.id;
     }
 };
-
-void printId(T &t); // declaration
-
-int T::cnt;
+int T::cnt = 0;
 
 int main()
 {
     ll n;
     T::ll m;
-    T m1,m2,m3,m4;
+    T m1("0"),m2("1"),m3("2"),m4;
     std::cout << m1.T_id() << m2.T_id() << m3.T_id() << m4.T_id() << std::endl;
     P::access_T(m1);
     return 0;
@@ -95,4 +117,8 @@ int main()
 
 void printId(T &t){ // defination
     std::cout << t.id << std::endl; // not work bc private without friend declaration
+}
+
+int T::T_id() const{
+    return this->id;
 }

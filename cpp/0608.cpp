4059d2b8305dc1aca1dbc21ab6d2780466214eda
nemoiwkryang <yanggr2025@shanghaitech.edu.cn>
2026-06-08 17:45:41 +0800

    0608 Update

--- cpp/0608.cpp
+++ cpp/0608.cpp
@@ -2,14 +2,14 @@
 lec 25, 26 - template
 */
 
-template <typename T>
-int cmp(const T&a, const T&b){
-    // const T& - T may not copyable, and const can accept rvalue
-    if(a<b) return -1;
-    if(b<a) return 1; // may not support '>'
+// template <typename T>
+// int cmp(const T&a, const T&b){
+//     // const T& - T may not copyable, and const can accept rvalue
+//     if(a<b) return -1;
+//     if(b<a) return 1; // may not support '>'
 
-    return 0;
-}
+//     return 0;
+// }
 /*
 - function templ
     - guideline for compile to deduce and generate function (instantiation)
@@ -159,6 +159,88 @@ class template
 */
 
 
+/*
+what if we want to compare char* and char*?
+*/
+
+#include<cstring>
+template <typename T>
+int cmp(const T&a, const T&b){
+    // const T& - T may not copyable, and const can accept rvalue
+    if(a<b) return -1;
+    if(b<a) return 1; // may not support '>'
+
+    return 0;
+}
+template <>
+int cmp<const char*>(const char*const &lhs, const char*const &rhs){
+    return std::strcmp(lhs,rhs);
+}
+
+int cmp(const char*const &lhs, const char*const &rhs){
+    return std::strcmp(lhs,rhs);
+}// this is function overload
+
+
+/*
+- specialization for function
+    - <const char*> can be omitted
+    - this is NOT overload
+    - however, this example is not ideal, as string literals will not call this one
+    - partial specialization not allowed
+        - e.g. cannot specialize a function accepting class templates, such as typename and container<typename>
+
+- "const ref" to ptr - high level constant: ...*const...
+- function template recurrence: compile time recurrence
+- operator: sizeof...(pack) - compile time
+- if constexpr: compile time if
+    - accepting a compile-time constant condition
+    - if constexpr (condition) statement1 else statement2
+    - e.g. used to prevent print() in our print function template, instead of overloading `print()`
+
+*/
+
+/*
+application: curiously recurring template pattern (CRTP, 奇异递归模板模式, 一个类把自己作为模板参数传给自己的基类。)
+- 1. to prevent copy/move, we can inherit from a uncopiable class
+    - but ref/ptr of base class can refer to totally different classes
+    - better implementation:
+        - idea: 1. template accept incomplete type; 2. <Type1> and <Type2> 2 are different types
+        - uncopyable template, and inherit Uncopyable<className>
+
+- 2. increment
+    - x++ is always defined: tmp; ++x; return tmp
+    - using template: just inherit a incrementable class template
+        - with tmp; ++x; return tmp
+        - use static_cast to change Base* this to Derived* this
+*/
+
+/*
+more about template metaprogramming
+    - template is_same_type
+        - partial specialize: <T,T> which gets a true
+    - template is_ptr
+        - also partial specialze: <T*>
+    - more these: stl, <type_traits>
+
+    ---
+
+    - compute factorial in compile time
+        - template class, static member = N * class<N-1>::member
+    - check prime using recursion and template
+
+    ---
+
+    physics - use basic quantities to represent other quantities
+        template <int mass, int length, int time, int charge,
+        int temperature, int intensity, int amount_of_substance>
+        struct quantity {};
+        (NTTP; different params instantiate different types)
+        using mass = quantity<1, 0, 0, 0, 0, 0, 0>;
+        using force = quantity<1, 1, -2, 0, 0, 0, 0>;
+        using pressure = quantity<1, -1, -2, 0, 0, 0, 0>;
+        using acceleration = quantity<0, 1, -2, 0, 0, 0, 0, 0>;
+*/
 int main()
 {
     std::vector a{1,2,3,4};
@@ -166,6 +248,6 @@ int main()
     for(auto &x: a)std::cout<<x <<" ";
     std::cout<<std::endl;
     print(1,2,3,4);
-
+    print(cmp(1,2),cmp("1","2"));
     return 0;
 }
